fb0467cc29fccd0a8cf0409dbf977de940937384
Bwarfson <wight.brad@gmail.com>
2018-02-28 16:55:24 -0700

    Test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -105,3 +105,39 @@ describe('GET /todos/:id', (params) => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', (done) => {
+    it('should remove a todo', (done) => {
+        const hexId = todos[1]._id.toHexString();
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        })
+        .end((err, res) => {
+            if(err) {
+                return done(err);
+            }
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toBeNull();
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        const id = new ObjectID().toHexString();
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+        .delete("/todos/123")
+        .expect(404)
+        .end(done);
+    });
+});
