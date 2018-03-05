179ef1dbca3c3c1844a442e7ecc5d4eb7e134d6a
Bwarfson <wight.brad@gmail.com>
2018-03-04 18:11:10 -0700

    Added token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var UserSchema = new mongoose.Schema({
     
@@ -31,7 +33,24 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
-//UserSchema.methods.generateAuthToken
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens = user.tokens.concat([{access, token}]);
+
+    return user.save().then(() => {
+        return token;
+    });
+};
 
 const User = mongoose.model('User', UserSchema);
 
